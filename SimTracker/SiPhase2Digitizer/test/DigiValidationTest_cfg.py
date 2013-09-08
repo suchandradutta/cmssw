df2650d6cc01e3852fa937eaa2aa55d4b392bcb9
Suchandra <Suchandra.Dutta@cern.ch mailto:Suchandra.Dutta@cern.ch>
2013-09-08 08:43:49 +0200

    adding BuildFile.xml

---
+++ SimTracker/SiPhase2Digitizer/test/DigiValidationTest_cfg.py
@@ -0,0 +1,52 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("digiTest")
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring('siPixelRawData'),
+    destinations = cms.untracked.vstring("cout"),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('ERROR')
+    )
+)
+process.source = cms.Source("PoolSource",
+    fileNames =  cms.untracked.vstring(
+       'file:/afs/cern.ch/work/d/dutta/public/Digitizer/CMSSW_6_1_2_SLHC5/src/prod/SingleMu_TK_BE.root'
+    )
+)
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.Geometry.GeometryExtendedPhase2TkBEReco_cff')
+process.load('Configuration.Geometry.GeometryExtendedPhase2TkBE_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedGauss_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'POSTLS161_V15::All', '')
+
+#-------------
+# Output ROOT file
+#-------------
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('./DigiValidation.root')
+)
+process.analysis = cms.EDAnalyzer("DigiValidation",
+    Verbosity = cms.untracked.bool(False),
+    src = cms.InputTag("simSiPixelDigis"),
+    simG4 = cms.InputTag("g4SimHits")                              
+)
+#process.digi_step = cms.Sequence(process.siPixelRawData*process.siPixelDigis)
+process.p = cms.Path(process.analysis)
