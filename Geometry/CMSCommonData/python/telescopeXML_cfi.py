054dbccbbb0b8039cc5df07391eeeb515a2d8a75
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2017-09-06 12:08:34 +0200

    Added first jet telescope XML (just one 2S module for the moment) + added associated geometry cfi file + it is now working and a geometry root file can be obtained

---
+++ Geometry/CMSCommonData/python/telescopeXML_cfi.py
@@ -0,0 +1,18 @@
+import FWCore.ParameterSet.Config as cms
+
+# This config was generated automatically using generate2023Geometry.py
+# If you notice a mistake, please update the generating script, not just this config
+
+XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
+    geomXMLFiles = cms.vstring(
+        'Geometry/CMSCommonData/data/materials.xml',
+        'Geometry/CMSCommonData/data/rotations.xml',
+        'Geometry/CMSCommonData/data/extend/cmsextent.xml',
+        'Geometry/CMSCommonData/data/cms.xml',
+        'Geometry/CMSCommonData/data/cmsMother.xml',
+        
+        'Geometry/CMSCommonData/data/telescope.xml'
+      
+    ),
+    rootNodeName = cms.string('cms:OCMS')
+)
