4deabfc4695ecb29f6b1385dfb98e31de004f8c2
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2017-12-21 10:59:55 +0100

    Geometry navigation and DetId: Part A. Added cff file + Define the geometry in Configuration/StandardSequences + Added topology paths in XML which should be useful for navigation (very introductory state right now, since obviously depends on the code).

--- Configuration/Generator/python/SingleMuPt1000_pythia8_cfi.py
+++ Configuration/Generator/python/SingleMuPt1000_pythia8_cfi.py
@@ -10,7 +10,7 @@ generator = cms.EDFilter("Pythia8PtGun",
         MinEta = cms.double(-2.5),
         MinPhi = cms.double(-3.14159265359) ## in radians
         ),
-                         Verbosity = cms.untracked.int32(0), ## set to 1 (or greater)  for printouts
+                         Verbosity = cms.untracked.int32(5), ## set to 1 (or greater)  for printouts
                          psethack = cms.string('single mu pt 1000'),
                          firstRun = cms.untracked.uint32(1),
                          PythiaParameters = cms.PSet(parameterSets = cms.vstring())
--- Geometry/TrackerPhase2TestBeam/python/Phase2TestBeamGeometryXML_cfi.py
+++ Geometry/TrackerPhase2TestBeam/python/Phase2TestBeamGeometryXML_cfi.py
@@ -1,29 +1,39 @@
 import FWCore.ParameterSet.Config as cms
 
 # This config was generated automatically using generate2023Geometry.py
-# If you notice a mistake, please update the generating script, not just this config
+# If you notice a mistake, please update the generating script, not just this config 
+
+# TO DO: update generate2023Geometry.py, or actually, create a different script.
 
 XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
     geomXMLFiles = cms.vstring(
          # World volume creation and default CMS materials
         'Geometry/CMSCommonData/data/materials.xml',
         'Geometry/CMSCommonData/data/rotations.xml',
-        'Geometry/TrackerPhase2TestBeam/data/cmsextent.xml',
-        'Geometry/TrackerPhase2TestBeam/data/cms.xml',
-        'Geometry/TrackerPhase2TestBeam/data/cmsMother.xml',
+        'Geometry/TrackerPhase2TestBeam/data/Common/cmsextent.xml',
+        'Geometry/TrackerPhase2TestBeam/data/Common/cms.xml',
+        'Geometry/TrackerPhase2TestBeam/data/Common/cmsMother.xml',
         
          # Define standalone Phase 1 BPIX module and associated materials
         'Geometry/TrackerCommonData/data/PhaseI/pixbarmaterial.xml',
         'Geometry/TrackerCommonData/data/Run2/trackermaterial.xml',   #sometimes included via Run2, sometimes via PhaseI, which one to choose?
         'Geometry/TrackerCommonData/data/PhaseI/pixfwdMaterials.xml',
-        'Geometry/TrackerPhase2TestBeam/data/Phase1BPIXLayer4Module.xml',
+        'Geometry/TrackerPhase2TestBeam/data/Common/Phase1BPIXLayer4Module.xml',
         
          # Define DUT and telescope
-        'Geometry/TrackerPhase2TestBeam/data/DUT.xml',
-        'Geometry/TrackerPhase2TestBeam/data/telescope.xml',
+        'Geometry/TrackerPhase2TestBeam/data/Common/DUT.xml',
+        'Geometry/TrackerPhase2TestBeam/data/Common/telescope.xml',
         
-         # Configurable parameters
-        'Geometry/TrackerPhase2TestBeam/data/Phase2BeamTestConstants.xml'
+         # Tunable geometry constants
+        'Geometry/TrackerPhase2TestBeam/data/Common/Phase2BeamTestConstants.xml',
+        
+        # Sim files
+        'Geometry/TrackerPhase2TestBeam/data/Sim/pixelTelescopeTopology.xml',
+        'Geometry/TrackerPhase2TestBeam/data/Sim/pixelTelescopeSensitive.xml',
+        'Geometry/TrackerPhase2TestBeam/data/Sim/pixelTelescopeProdCuts.xml',
+        
+        # Reco file
+        'Geometry/TrackerPhase2TestBeam/data/Reco/pixelTelescopeRecoMaterial.xml'
       
     ),
     rootNodeName = cms.string('cms:OCMS')
