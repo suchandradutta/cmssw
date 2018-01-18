58a10def912f3dad207a681508213ecccb43d7e2
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2018-01-18 18:53:18 +0100

    fixed compilation (actually might not be necessary, to be clean to minimal needs)

--- Geometry/TrackerGeometryBuilder/src/module.cc
+++ Geometry/TrackerGeometryBuilder/src/module.cc
@@ -1,10 +1,12 @@
 #include "DataFormats/TrackerCommon/interface/TelescopeTopology.h"
 #include "Geometry/TrackerPhase2TestBeam/interface/PTelescopeParameters.h"
+#include "Geometry/Records/interface/PTelescopeParametersRcd.h"
 
 #include "FWCore/Utilities/interface/typelookup.h"
 
 
 TYPELOOKUP_DATA_REG(TelescopeTopology);
 TYPELOOKUP_DATA_REG(PTelescopeParameters);
+TYPELOOKUP_DATA_REG(PTelescopeParametersRcd);
 
 
